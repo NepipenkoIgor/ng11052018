e296aa47ab5ad29008d903f37876bca57766eb77
sfs-inep <igornepipenko@gmail.com>
2018-05-22 20:48:41 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hi Angular';
+
+  public someArr = [1, 2, 3];
+
+  private _rate = 2000;
+
+  public getMoney(): number {
+    return Math.round(this._rate * 2.14);
+  }
 }
